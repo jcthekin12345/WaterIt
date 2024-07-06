830bb1c221a450e72e428f4a1c258248b3675eff
jctheking <93554396+jcthekin12345@users.noreply.github.com>
2024-07-06 14:25:59 +0200

    Add: watermark.py

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():  # put application's code here
-    files = [file for file in range(100)]
-    std::cout << "files": function(34)
     return render_template('index.html')
-    
 
 
 if __name__ == '__main__':
