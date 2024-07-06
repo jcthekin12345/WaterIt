e84310c410a05d486b8733c6ae9afc8aa3fda982
jctheking <93554396+jcthekin12345@users.noreply.github.com>
2024-07-06 14:32:14 +0200

    Modified: made starting function for watermark

--- watermark.py
+++ watermark.py
@@ -1,1 +1,11 @@
+from PIL import Image
 
+def watermark(image_path, watermark_path, output_path):
+    with open(image_path, 'rb') as f:
+        image = Image.open(f)
+
+
+
+
+if __name__ == '__main__':
+    pass
